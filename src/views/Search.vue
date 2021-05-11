5c4f65176cc288366180fb6c928b285cc7c50b6e
trisub49 <trisub49@gmail.com>
2021-05-11 11:57:10 +0200

    search box size and position changed

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,56 +1,58 @@
 <template>
   <v-container class="search">
     <MainPageStructure title="Keresés">
-      <v-expansion-panels v-model="panel" multiple>
-        <v-expansion-panel expand :readonly="searchState != 2">
-          <v-expansion-panel-header> 
-            <h3>Keresési feltételek</h3>
-          </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <v-form id="searchform">
-                <v-text-field
-                  prepend-icon="mdi-magnify"
-                  label="Kulcsszó"
-                  v-model="scope"
-                  placeholder="kőműves, java programozó, stb..."
-                  autofocus
-                  @mouseover="resetAlerts()"
-                />
-                <CategoryList />
-                <v-text-field
-                  prepend-icon="mdi-animation-outline"
-                  label="Kategória"
-                  v-model="category"
-                  list="categorylist"
-                  placeholder="IT, könyvelés, stb..."
-                  @mousever="resetAlerts()"
-                />
-                <SettlementList />
-                <v-text-field
-                  prepend-icon="mdi-map-marker"
-                  label="Hely"
-                  v-model="postcode"
-                  list="settlementlist"
-                  placeholder="pl.: Budapest"
-                  @mouseover="resetAlerts()"
-                />
-              </v-form>
-              <v-container id="alerts" class="justify-center">
-                <v-alert v-if="noCondition" type="error" dense outlined class="text-center">
-                  Nem adtál meg keresési feltételt!
-                </v-alert>
-                <v-alert v-if="searchState == 1" type="info" dense outlined class="text-center">
-                  A feltételek alapján nincs találat!
-                </v-alert>
-              </v-container>
-              <v-container class="text-center">
-                  <v-btn class="mainbutton" width="50%" @click="search()">
-                    <v-icon>mdi-magnify</v-icon>Keresés
-                  </v-btn>
+      <v-container id="exp" class="justify-center">
+        <v-expansion-panels v-model="panel" multiple>
+          <v-expansion-panel expand :readonly="searchState != 2">
+            <v-expansion-panel-header> 
+              <h3>Keresési feltételek</h3>
+            </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-form id="searchform">
+                  <v-text-field
+                    prepend-icon="mdi-magnify"
+                    label="Kulcsszó"
+                    v-model="scope"
+                    placeholder="kőműves, java programozó, stb..."
+                    autofocus
+                    @mouseover="resetAlerts()"
+                  />
+                  <CategoryList />
+                  <v-text-field
+                    prepend-icon="mdi-animation-outline"
+                    label="Kategória"
+                    v-model="category"
+                    list="categorylist"
+                    placeholder="IT, könyvelés, stb..."
+                    @mousever="resetAlerts()"
+                  />
+                  <SettlementList />
+                  <v-text-field
+                    prepend-icon="mdi-map-marker"
+                    label="Hely"
+                    v-model="postcode"
+                    list="settlementlist"
+                    placeholder="pl.: Budapest"
+                    @mouseover="resetAlerts()"
+                  />
+                </v-form>
+                <v-container id="alerts" class="justify-center">
+                  <v-alert v-if="noCondition" type="error" dense outlined class="text-center">
+                    Nem adtál meg keresési feltételt!
+                  </v-alert>
+                  <v-alert v-if="searchState == 1" type="info" dense outlined class="text-center">
+                    A feltételek alapján nincs találat!
+                  </v-alert>
                 </v-container>
-            </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+                <v-container class="text-center">
+                    <v-btn class="mainbutton" width="50%" @click="search()">
+                      <v-icon>mdi-magnify</v-icon>Keresés
+                    </v-btn>
+                  </v-container>
+              </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-container>
       <v-container v-if="searchState == 2">
         <v-spacer />
         <span id="foundcounter"><h3>{{foundJobs.length}} találat</h3></span>
@@ -62,6 +64,9 @@
 </template>
 
 <style scoped>
+#exp {
+  max-width: 640px;
+}
 #foundcounter {
   color: green;
 }
